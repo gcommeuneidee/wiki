ba4775160d3927248705e2137701804be9ee8f37
PacomeGcom <pacome.adam@gcommeuneidee.com>
2022-06-10 11:35:57 +0200

    changement url deploiement

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://gcommeuneidee.github.io',
+  url: 'https://github.com',
   baseUrl: '/wiki/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
